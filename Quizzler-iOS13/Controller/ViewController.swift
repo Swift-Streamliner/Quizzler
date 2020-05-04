838adacf789ca1fced983c0954711baf1f3e8953
Tiger <thisisstreamliner@gmail.com>
2020-05-04 12:49:35 +0300

    refactoring project to MVC

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.  
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle! // True, False
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+            print("Right")
+        } else {
+            sender.backgroundColor = UIColor.red
+            print("Wrong")
+        }
+        quizBrain.nextQuestion() 
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+    }
+     
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestinText() 
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = quizBrain.getProgress()
+    }
+    
+}
+
